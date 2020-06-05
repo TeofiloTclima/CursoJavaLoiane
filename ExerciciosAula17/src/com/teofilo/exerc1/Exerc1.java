b1a412d2f02a6fc62cda8aac5cba1f2ad38019fa
TeofiloTclima <teofilotclima@gmail.com>
2020-06-04 22:56:38 -0300

    Exercicio 1 ok

--- ExerciciosAula17/src/com/teofilo/exerc1/Exerc1.java
+++ ExerciciosAula17/src/com/teofilo/exerc1/Exerc1.java
@@ -1,8 +1,28 @@
 package com.teofilo.exerc1;
 
+import java.util.Scanner;
 public class Exerc1 {
 
     public static void main(String[] args) {
-	System.out.println("Teste");
+        Scanner scan = new Scanner(System.in);
+
+        boolean notaValida = false;
+
+        do{
+
+            System.out.println("Entre com uma nota");
+
+            double nota = scan.nextDouble();
+
+                if (nota >=0 &&  nota <=10){
+                    notaValida = true;
+                System.out.println("Você digitou: " + nota);
+                }else{
+                System.out.println("nota inválida, digite novamente.");
+                }
+        }while(! notaValida);
+
+
     }
+
 }
