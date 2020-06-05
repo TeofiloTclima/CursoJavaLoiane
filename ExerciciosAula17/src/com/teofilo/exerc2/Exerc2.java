37ab83bbeb84d635446bd3b2c293c1e8570a9a4e
Teofilo <teofilotclima@gmail.com>
2020-06-05 18:37:14 -0300

    Exercicio 2.3 ok

--- ExerciciosAula17/src/com/teofilo/exerc2/Exerc2.java
+++ ExerciciosAula17/src/com/teofilo/exerc2/Exerc2.java
@@ -1,28 +1,26 @@
 package com.teofilo.exerc2;
-
-
 import java.util.Scanner;
 
 public class Exerc2 {
-    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-    boolean infoValidas = false;
+        boolean infoValidas = false;
 
-    do{
-        System.out.println("Digite o nome do usuário");
-        String nomeUser = scan.next();
+        do {
+            System.out.println("Digite o nome do usuário");
+            String nomeUser = scan.next();
 
-        System.out.println("Entre com a senha");
-        String senha = scan.next();
+            System.out.println("Entre com a senha");
+            String senha = scan.next();
 
-        if (nomeUser.equalsIgnoreCase(senha)){
-
-            System.out.println("Senha igual a usuário, digite novamente.");
-        }else {
-            infoValidas = true;
-            System.out.println("Senha e usuário válido");
-        }
-    }while(! infoValidas);
-
-
+            if (nomeUser.equalsIgnoreCase(senha)) {
+                //infoValidas = false;
+                System.out.println("Senha igual a usuário, digite novamente.");
+            } else {
+                infoValidas = true;
+                System.out.println("Senha e usuário válido");
+            }
+        } while (!infoValidas);
+    }
 }
