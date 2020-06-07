382d27eebdb34ab090e0b58c39c7ee720ec46632
Teofilo <teofilotclima@gmail.com>
2020-06-07 15:29:57 -0300

    Exercicio 6.1 ok

---
+++ ExerciciosAula17/src/com/teofilo/Exerc6/Exerc6.java
@@ -0,0 +1,11 @@
+package com.teofilo.Exerc6;
+
+public class Exerc6 {
+    public static void main(String[] args){
+
+        for (int i=1; i<= 20; i++){
+            //System.out.println(i);
+            System.out.print(i);
+        }
+    }
+}
