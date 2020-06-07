8c62b15c6bdb97c73e222f9ce57bb515355551ea
Teofilo <teofilotclima@gmail.com>
2020-06-07 13:49:49 -0300

    Exercicio 3.1 ok

---
+++ ExerciciosAula17/src/com/teofilo/exerc3/Exerc3.java
@@ -0,0 +1,77 @@
+package com.teofilo.exerc3;
+import java.util.Scanner;
+
+public class Exerc3 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        boolean infoValida = false;
+        String nome, sexo, estadoCivil;
+        int idade;
+        double salario;
+
+        do{
+            System.out.println("Entre com o nome:");
+            nome = scan.next();
+
+            if (nome.length() >3){
+                    infoValida =true;
+            }else {
+                    System.out.println("Nome precisa ter no mínimo 3 caracteres.");
+            }
+        }while (!infoValida);
+
+        boolean infovalida = false;
+        do{
+            System.out.println("Entre com a idade:");
+            idade = scan.nextInt();
+
+            if (idade >= 0 && idade <=150){
+                    infoValida =true;
+            }else {
+                    System.out.println("Idade precisa ser entre 0 e 150..");
+            }
+        }while (!infoValida);
+
+         infovalida = false;
+        do{
+            System.out.println("Entre com a idade:");
+            salario = scan.nextDouble();
+
+            if (salario > 0 ){
+                infoValida =true;
+            }else {
+                System.out.println("Salário precisa ser maior que 0.");
+            }
+        }while (!infoValida);
+
+        infovalida = false;
+        do{
+            System.out.println("Entre com a idade:");
+            sexo = scan.next();
+
+            if (sexo.equalsIgnoreCase("f") ||
+                    sexo.equalsIgnoreCase("m")){
+                infoValida =true;
+            }else {
+                System.out.println("Sexo precisa ser 'f' para Feminino ou 'm' para masculino.");
+            }
+        }while (!infoValida);
+
+        infovalida = false;
+        do{
+            System.out.println("Entre com a idade:");
+            estadoCivil = scan.next();
+
+            if (estadoCivil.equalsIgnoreCase("s") ||
+                    estadoCivil.equalsIgnoreCase("c")||
+                    estadoCivil.equalsIgnoreCase("v")||
+                    estadoCivil.equalsIgnoreCase("d")){
+                infoValida =true;
+            }else {
+                System.out.println("Estado Civil precisa ser 's' solteiro, 'c' casado, 'v' viuvo e 'd' para divorciado:");
+            }
+        }while (!infoValida);
+    }
+
+}
