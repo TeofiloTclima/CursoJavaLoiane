36a2e23633cba089a7afcf246f414fea6d39c4be
Teofilo <teofilotclima@gmail.com>
2020-06-07 20:49:31 -0300

    commit Exerc14 ok

---
+++ ExerciciosAula17/src/com/teofilo/EXerc14/Exerc14.java
@@ -0,0 +1,25 @@
+package com.teofilo.EXerc14;
+import java.util.Scanner;
+
+public class Exerc14{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num;
+        int pares =0;
+        int impares =0;
+
+        for(int i =0; i<10; i++){
+            System.out.println("Entre com um número");
+            num = scan.nextInt();
+
+            if (num % 2 == 0){
+                pares++;
+            }else{
+                impares++;
+            }
+        }
+        System.out.println("Pares: " + pares);
+        System.out.println("Impares: " + impares);
+    }
+
+}
