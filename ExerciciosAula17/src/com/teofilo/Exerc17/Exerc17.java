a546ab8366144cf6b482a285de3bfc3a46479efc
Teofilo <teofilotclima@gmail.com>
2020-06-11 13:08:43 -0300

    commit Exerc17 ok

---
+++ ExerciciosAula17/src/com/teofilo/Exerc17/Exerc17.java
@@ -0,0 +1,23 @@
+package com.teofilo.Exerc17;
+
+import java.util.Scanner;
+
+public class Exerc17 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite um número:");
+        int num = scan.nextInt();
+
+        System.out.print(num + "! = ");
+
+        int fatorial = 1;
+        for (int i=num; i>0; i--){
+            fatorial *= i;
+            System.out.println(i);
+
+        }
+        System.out.println("Resultato: " + fatorial);
+    }
+
+}
