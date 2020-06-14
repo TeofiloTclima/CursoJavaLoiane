68ba3adc1da285d613986387def7f6af957f4a9c
Teofilo <teofilotclima@gmail.com>
2020-06-14 16:57:25 -0300

    commit Aula18 ok

---
+++ CursoLoiane_Aula18/src/com/teofilo/Aula18/Continue.java
@@ -0,0 +1,23 @@
+package com.teofilo.Aula18;
+
+import java.util.Scanner;
+
+public class Continue {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com um número");
+        int num = scan.nextInt();
+
+        System.out.println("Entre com um limite");
+        int max = scan.nextInt();
+
+        for (int i =num; i< max; i++){
+
+            if (i % 7 == 0){
+                continue;
+            }
+            System.out.println("O valor de i é: " + i);
+        }
+    }
+}
