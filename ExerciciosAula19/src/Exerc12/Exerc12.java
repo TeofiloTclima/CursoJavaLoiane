fc738ba52579047e8256cea3c0c45c3a0e8c47a9
teofilo <teofilotclima@gmail.com>
2020-06-24 19:02:53 -0300

    commit Exerc12 ok

---
+++ ExerciciosAula19/src/Exerc12/Exerc12.java
@@ -0,0 +1,28 @@
+package Exerc12;
+
+import java.util.Scanner;
+
+public class Exerc12 {
+    public  static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+
+        for (int i=0; i<vetorA.length; i++){
+            System.out.println("Entre com o valor da posição " + i);
+            vetorA[i] = scan.nextInt();
+        }
+
+        int soma =0;
+        for (int i=0; i<vetorA.length; i++){
+        soma+= vetorA[i];
+        }
+        System.out.println("Vetor A = ");
+        for (int i=0; i<vetorA.length; i++){
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Soma: " + soma);
+    }
+}
