45c58e5a7757ec78b976409baa0f217d1ace4c1d
teofilo <teofilotclima@gmail.com>
2020-06-24 16:08:09 -0300

    commit Exerc10 ok

--- ExerciciosAula19/src/Exerc10/Exerc10.java
+++ ExerciciosAula19/src/Exerc10/Exerc10.java
@@ -9,6 +9,27 @@ import java.util.Scanner;
 public class Exerc10 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+
+        int vetorA[] = new int[10];
+        int vetorB[] = new int[vetorA.length];
+
+        for (int i=0; i<vetorA.length; i++){
+            System.out.println("Entre com o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+
+            vetorB[i] = vetorA[i] % 2;
+        }
+        System.out.print("Vetor A=");
+        for (int i=0; i<vetorA.length; i++){
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor B=");
+        for (int i=0; i<vetorB.length; i++){
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
     }
 
 }
